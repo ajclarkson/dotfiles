aa9d881dc8c90c60b7cbfaa7355fd4c5f2f2c4a4
Adam Clarkson <adam@ajclarkson.co.uk>
2023-06-02 18:30:31 +0100

    Add wifi-password util

--- scripts/brew.sh
+++ scripts/brew.sh
@@ -6,7 +6,18 @@ log_start "Installing brew and common programs"
 
 brew -v >/dev/null 2>&1 || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
-brew_install_or_upgrade zsh
+for app in "zsh" \
+  "nvm" \
+  "exa" \
+  "curl" \
+  "jq" \
+  "bat" \
+  "terraform" \
+  "wget" \
+  "mas" \
+  "wifi-password"; do
+  brew_install_or_upgrade $app
+done
 
 # Set zsh as the default shell
 if [ $SHELL = "/bin/zsh" ]; then
@@ -15,14 +26,5 @@ else
     echo "Setting default shell to zsh"
     chsh -s /bin/zsh
 fi
-
-brew_install_or_upgrade nvm
-brew_install_or_upgrade exa
-brew_install_or_upgrade curl
-brew_install_or_upgrade jq
-brew_install_or_upgrade bat
-brew_install_or_upgrade terraform
-brew_install_or_upgrade wget
-brew_install_or_upgrade mas
 
 log_success "Successfully installed brew and common programs"
