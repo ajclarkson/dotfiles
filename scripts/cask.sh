c209952e0a06371e84f32c44acf3377a5933cdc6
Adam Clarkson <adam@ajclarkson.co.uk>
2023-06-02 17:02:14 +0100

    Add vlc and arq to home cask dependencies

--- scripts/cask.sh
+++ scripts/cask.sh
@@ -11,39 +11,49 @@ brew tap homebrew/cask-fonts
 brew install --cask font-fira-code
 brew install --cask font-caskaydia-cove-nerd-font
 
-brew install --cask spotify
-brew install --cask obsidian
-brew install --cask iterm2
+
+for app in "raycast" \
+  "bartender" \
+  "logitune" \
+  "whatsapp" \
+  "appcleaner" \
+  "rectangle" \
+  "google-chrome" \
+  "1password" \
+  "zwift" \
+  "visual-studio-code" \
+  "spotify" \
+  "obsidian" \
+  "iterm2"; do
+  brew install --cask $app
+done
+
 defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "$CONFIG_DIR/iterm"
 defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
 
-brew install --cask raycast
-brew install --cask bartender
-brew install --cask logitune
-brew install --cask whatsapp
-brew install --cask appcleaner
-brew install --cask rectangle
-brew install --cask google-chrome
-brew install --cask 1password
-brew install --cask zwift
-
-brew install --cask visual-studio-code
 cat $CONFIG_DIR/vscode/extensions.txt | while read ext; do
     code --install-extension $ext
 done 
 symlink $CONFIG_DIR/vscode/settings.json $HOME/Library/Application\ Support/Code/User/settings.json
 
 if [ $SETUP_MODE == "work" ]; then
-    brew install --cask slack
-    brew install --cask meetingbar
+  for work_app in "slack" \
+    "meetingbar"; do
+    brew install --cask $work_app
+  done
+
 fi
 
 if [ $SETUP_MODE == "home" ]; then
-    brew install --cask lens
-    brew install --cask mqttx
-    brew install --cask nordvpn
-    brew install --cask qmk-toolbox
-    brew install --cask openvpn-connect
+    for home_app in "lens" \
+    "mqttx" \
+    "nordvpn" \
+    "qmk-toolbox" \
+    "openvpn-connect" \
+    "arq" \
+    "vlc"; do
+    brew install --cask $home_app
+    done
 fi
 
 log_success "Succesfully installed applications via cask"
