80213375dfed9bc253c0b38651deb7af1e89c807
Adam Clarkson <adam@ajclarkson.co.uk>
2023-06-27 09:37:03 +0100

    Add some primeagen remaps for navigating

--- config/nvim/lua/ajclarkson/remap.lua
+++ config/nvim/lua/ajclarkson/remap.lua
@@ -1,7 +1,6 @@
 
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
-
 -- Move the highlighted lines up and down in the file
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '>-2<CR>gv=gv")
@@ -11,3 +10,12 @@ vim.keymap.set("n", "<leader>y", "\"+y")
 vim.keymap.set("v", "<leader>y", "\"+y")
 vim.keymap.set("n", "<leader>Y", "\"+Y")
 vim.keymap.set("v", "<leader>Y", "\"+Y")
+
+-- Keep the cursor in the middle when half page jumping or searching
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- Paste over the top of a buffer without taking the contents to the register
+vim.keymap.set("x", "<leader>p", "\"_dP")
