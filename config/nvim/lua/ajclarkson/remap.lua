d6964c20a5634219042bcf255b83680f7c237c92
Adam Clarkson <adam@ajclarkson.co.uk>
2023-08-15 10:41:38 +0100

    Bring back meetingbar, better than the raycast calendar integration when using gcal

--- config/nvim/lua/ajclarkson/remap.lua
+++ config/nvim/lua/ajclarkson/remap.lua
@@ -2,7 +2,7 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 -- Move the highlighted lines up and down in the file
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '>-2<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 -- Yank files into the system clipboard register rather than vim
 vim.keymap.set("n", "<leader>y", "\"+y")
