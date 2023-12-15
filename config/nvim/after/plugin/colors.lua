88610149aec878fb08695e66373f630fa8892527
ajclarkson <adam@ajclarkson.co.uk>
2023-12-15 11:33:51 +0000

    Fix linting problems

--- config/nvim/after/plugin/colors.lua
+++ config/nvim/after/plugin/colors.lua
@@ -1,10 +1,9 @@
-function SetColors(color) 
+function SetColors(color)
 	color = color or "kanagawa"
 	vim.cmd.colorscheme(color)
 
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
         vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-	
 end
 
 SetColors()
