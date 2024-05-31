219dab3288f711ac034e93e0aecd6385393d3f11
Adam Clarkson <adam@ajclarkson.co.uk>
2024-05-31 12:26:21 +0100

    Formatting fixes

--- config/nvim/lua/ajclarkson/lazy/neotree.lua
+++ config/nvim/lua/ajclarkson/lazy/neotree.lua
@@ -1,7 +1,7 @@
 return {
     'nvim-neo-tree/neo-tree.nvim',
     branch = 'v3.x',
-    dependencies = { 
+    dependencies = {
         'nvim-lua/plenary.nvim',
         'nvim-tree/nvim-web-devicons',
         'MunifTanjim/nui.nvim',
