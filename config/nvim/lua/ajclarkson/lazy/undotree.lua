edb2e72c382ebe74de80b851b7f6ad84192283d6
Adam Clarkson <adam@ajclarkson.co.uk>
2024-05-31 12:25:31 +0100

    Update undotree config

--- config/nvim/lua/ajclarkson/lazy/undotree.lua
+++ config/nvim/lua/ajclarkson/lazy/undotree.lua
@@ -2,5 +2,6 @@ return {
     'mbbill/undotree',
     config = function()
         vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
+        vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
     end
 }
