29921e29e26cb519b4a0bbadabe712b0e472b9a2
Adam Clarkson <adam@ajclarkson.co.uk>
2024-11-12 11:41:29 +0000

    Disable auto turn on off noneckpain when on a markdown file

--- config/nvim/lua/ajclarkson/autocmds.lua
+++ config/nvim/lua/ajclarkson/autocmds.lua
@@ -3,11 +3,3 @@ local ajclarksonGroup = augroup('ajclarksonGroup', {})
 
 local autocmd = vim.api.nvim_create_autocmd
 
--- Turn no neck pain plugin on automatically for markdown files
-autocmd('FileType', {
-    group = ajclarksonGroup,
-    pattern = { "markdown", "*.md" },
-    callback = function ()
-        require('no-neck-pain').enable()
-    end
-})
