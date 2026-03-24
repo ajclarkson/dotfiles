ede75106e5c159cd01f21606906cce708b319c78
adam.clarkson <adam.clarkson@ocado.com>
2026-03-24 16:28:02 +0000

    Tidy up fish config

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,4 +2,6 @@ source ~/.config/fish/ajclarkson/abbr.fish
 source ~/.config/fish/ajclarkson/variables.fish
 source ~/.config/fish/ajclarkson/paths.fish
 source ~/.config/fish/ajclarkson/prompt.fish
+source ~/.config/fish/ajclarkson/fnm.fish
+source ~/.config/fish/ajclarkson/secrets.fish
 fish_config theme choose "rose-pine"
